17393b220e7e52ab121db855bc8f57980fece35b
Zhu Yu <mike.zhuyu@gmail.com>
2019-04-11 23:13:06 +0800

    Update

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"strconv"
 )
 
+// 京版小英二下课本完整音轨.mp3
 var mp3parts = []string{
 	"00:00",
 	"01:49",
@@ -40,25 +40,59 @@ var mp3parts = []string{
 	"48:27", // the total length
 }
 
+// 京版小英四下课本完整音轨
+var mp3parts1 = []string{
+	"00:00",
+	"03:30",
+	"06:08",
+	"07:58",
+	"10:30",
+	"13:19",
+	"15:33",
+	"17:47",
+	"19:45",
+	"23:19",
+	"25:10",
+	"27:00",
+	"29:18",
+	"32:50",
+	"35:56",
+	"38:10",
+	"41:25",
+	"43:13",
+	"45:25",
+	"47:25",
+	"49:25",
+}
+
 func main() {
 	var inputFile string
 	var outPath string
 
-	flag.StringVar(&inputFile, "input", "京版小英二下课本完整音轨.mp3", "input file")
+	flag.StringVar(&inputFile, "input", "京版小英四下课本完整音轨.mp3", "input file")
 	flag.StringVar(&outPath, "outdir", ".", "output path")
 	flag.Parse()
 
-	for i := 0; i < len(mp3parts)-1; i++ {
-		start := mp3parts[i]
-		end := mp3parts[i+1]
+	parts := mp3parts1
+
+	for i := 0; i < len(parts)-1; i++ {
+		start := parts[i]
+		end := parts[i+1]
+
+		if len(start) == 5 {
+			start = "00:" + start
+		}
+		if len(end) == 5 {
+			end = "00:" + end
+		}
 
 		// ffmpeg -i 京版小英二下课本完整音轨.mp3 -ss 00:03:24 -to 00:05:11 -acodec copy L3.mp3
 		cmd := exec.Command("/usr/local/sbin/ffmpeg",
 			"-i", inputFile,
-			"-ss", "00:"+start,
-			"-to", "00:"+end,
+			"-ss", start,
+			"-to", end,
 			"-acodec", "copy",
-			outPath+"/L"+strconv.Itoa(i+1)+".mp3")
+			fmt.Sprintf("%s/L%02d.mp3", outPath, (i+1)))
 		var out bytes.Buffer
 		cmd.Stdout = &out
 
